9a1370a000fdb13c4716563799a3a5493f14a555
Jared Woolston <Jared.Woolston@gmail.com>
2017-10-15 20:57:36 -0700

    First transfer of project files from Rajawali repository
    Signed-off-by: Jared Woolston <Jared.Woolston@gmail.com>

---
+++ .travis.compile.sh
@@ -0,0 +1,5 @@
+set -e
+
+docker run -it -u `stat -c "%u:%g" .` -v ~/.aws:/root/.aws -v ${TRAVIS_BUILD_DIR}:/workspace -w /workspace
+rajawali/alpine-glibc-android:beta-feature_ndk-r16 /bin/sh -c \
+    "./gradlew build test --stacktrace --continue --console=plain"
